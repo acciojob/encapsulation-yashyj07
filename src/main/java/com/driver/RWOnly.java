93eb620779c22fbbaf2a2ece845cb835fe73cd75
Yash Jivani <yashyj1908@gamil.com>
2023-08-09 11:13:20 +0530

    Attempt - 3 made functions as public

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,11 +3,11 @@ package com.driver;
 public class RWOnly {
     private String name;
 
-    void setName(String name){
+    public void setName(String name){
         this.name = name;
     }
 
-    void getName(){
+    public void getName(){
         System.out.println(this.name);
     }
 }
