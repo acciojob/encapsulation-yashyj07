138a1f86989a2a0d55f3f631cbd6e67e1ddd12c7
Yash Jivani <yashyj1908@gamil.com>
2023-08-09 10:49:19 +0530

    Assignment Done - Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+
+
+        /*
+        rw.name = "Test";
+        System.out.println(name);
+
+        Getting below-mentioned error after Task-4:
+            java: name has private access in com.driver.RWOnly
+        */
+
+        rw.setter("Test");
+        rw.getter();
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    void setter(String name){
+        this.name = name;
+    }
+
+    void getter(){
+        System.out.println(this.name);
+    }
 }
